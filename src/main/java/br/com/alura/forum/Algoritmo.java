f576af0ead6bf52af2b87af92376b6f2da521c16
guithepc <guilherme.pc@yahoo.com>
2021-08-18 07:52:34 -0300

    second commit

---
+++ src/main/java/br/com/alura/forum/Algoritmo.java
@@ -0,0 +1,17 @@
+package br.com.alura.forum;
+
+import java.util.Iterator;
+
+public class Algoritmo {
+
+	public static void main(String[] args) {
+		int[] vet = {4, 3, 2, 9, 3, 8};
+		int aux = 0;
+		int i1 = 0;
+		
+		for (int i = 0; i < vet.length; i++) {
+			System.out.println("Vetor desordenado: "+vet[i]);
+		} 
+		
+	}
+}
